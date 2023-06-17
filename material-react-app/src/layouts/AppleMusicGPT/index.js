ca3c59a308dc7197f7b86f61a678d13c8cd49a81
kwill5800 <kwill5800@gmail.com>
2023-06-17 15:32:05 -0700

    authorize and unauthorize

--- material-react-app/src/layouts/AppleMusicGPT/index.js
+++ material-react-app/src/layouts/AppleMusicGPT/index.js
@@ -31,45 +31,75 @@ import MDButton from "components/MDButton";
 import GPTService from 'services/gpt-service';
 import Stack from '@mui/material/Stack';
 import { DataGrid } from '@mui/x-data-grid';
-const sendData = async () => {
 
-  const payload = {
-    prompt: "Make me a 5 song r and b playlist"
-  };
-
-  try {
-    const response = await GPTService.generate(JSON.stringify(payload));
-    setResponseData(response); // Do something with the response
-  } catch (error) {
-    console.error(error);
-  }
-};
 function AppleMusicGPT() {
+  const [input, setInput] = useState("");
+  const [response, setResponseData] = useState(null);
+  const [music, setMusic] = useState(null);  
+
+  const handleAuthorize = () => {
+    try {
+      window.MusicKit.configure({
+        developerToken: 'eyJhbGciOiJFUzI1NiIsImtpZCI6IjY3TDY2U05QTTIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJVN1dLODZQTFlYIiwiaWF0IjoxNjg3MDEzODMyLCJleHAiOjE3MDE0MTM4MzJ9.dxtV-q4w9aJgAe5MqaUeK5RbqcWYXVo6QT_MU1SA6gEx2gNscENfuhq6QajgnMV-yqYq4P2QmwJUnIgpNcGxAw',
+        app: {
+          name: 'My Cool Web App',
+          build: '1978.4.1',
+        },
+      });
+
+      // store the instance in a state or in a React context
+    } catch (err) {
+      console.error('MusicKit configuration error:', err);
+    }
+    const music = MusicKit.getInstance();
+    music.authorize()
+      .then(() => {
+        console.log('User authorized');
+        console.log(music.musicUserToken);
+      })
+      .catch((error) => {
+        console.error('Authorization error:', error);
+      });
+  };
+  const handleUnauthorize = () =>{
+    try {
+      window.MusicKit.configure({
+        developerToken: 'eyJhbGciOiJFUzI1NiIsImtpZCI6IjY3TDY2U05QTTIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJVN1dLODZQTFlYIiwiaWF0IjoxNjg3MDEzODMyLCJleHAiOjE3MDE0MTM4MzJ9.dxtV-q4w9aJgAe5MqaUeK5RbqcWYXVo6QT_MU1SA6gEx2gNscENfuhq6QajgnMV-yqYq4P2QmwJUnIgpNcGxAw',
+        app: {
+          name: 'My Cool Web App',
+          build: '1978.4.1',
+        },
+      });
+
+      // store the instance in a state or in a React context
+    } catch (err) {
+      console.error('MusicKit configuration error:', err);
+    }
+    const music = MusicKit.getInstance();
+    music.unauthorize();
+    console.log('User unauthorized');
+  }
+
   const sendData = async () => {
 
     const payload = {
-      prompt: "Make me a 5 song r and b playlist"
+      prompt: input 
     };
   
     try {
       const response = await GPTService.generate(JSON.stringify(payload));
       setResponseData(response); // Do something with the response
+      setInput("");
     } catch (error) {
       console.error(error);
     }
   };
-  const [isGenerated, setIsGenerated] = useState(false);
-  // State to hold response data
-  const [response, setResponseData] = useState(null);
-  useEffect(() => {
-    // If button has been clicked, call sendData
-    if(isGenerated) {
-      sendData(setResponseData);
-    }
-  }, [isGenerated]);
+
+
   const columns = [
-    { field: 'song', headerName: 'Song Name', width: 200 },
-    { field: 'artist', headerName: 'Artist', width: 200 },
+    { field: 'song', headerName: 'Song Name', width: 250 },
+    { field: 'artist', headerName: 'Artist', width: 250 },
+    
   ];
 
   // Ma to rows for DataGrid
@@ -89,7 +119,15 @@ function AppleMusicGPT() {
 >
       <Grid container spacing={3}>
       <Grid item xs={7}>
-      <MDInput label="Enter Your Prompt" multiline rows={5} fullWidth/>
+      <MDInput 
+  label="Enter Your Prompt" 
+  multiline 
+  rows={5} 
+  fullWidth
+  value={input}
+  onChange={event => setInput(event.target.value)}
+  
+/>
       </Grid>
       <Grid item xs={5}>
       <MDBox>
@@ -98,12 +136,35 @@ function AppleMusicGPT() {
                   color="info"
                   fullWidth
                   type="button"
-                  onClick={() => setIsGenerated(true)} 
+                  onClick={sendData} 
                 >
                   Generate
                 </MDButton>
               </MDBox>
+              <MDBox>
+                <MDButton
+                  variant="gradient"
+                  color="info"
+                  fullWidth
+                  type="button"
+                  onClick={handleAuthorize} 
+                >
+                  Authenticate
+                </MDButton>
+              </MDBox>
+              <MDBox>
+                <MDButton
+                  variant="gradient"
+                  color="info"
+                  fullWidth
+                  type="button"
+                  onClick={handleUnauthorize} 
+                >
+                  UnAuthenticate
+                </MDButton>
+              </MDBox>
       </Grid>
+      
     </Grid>
         
         <MDBox>
