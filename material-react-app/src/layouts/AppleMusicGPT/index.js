ce10d29835218d83a3077a9f131b3d692c64beaf
kwill5800 <kwill5800@gmail.com>
2023-06-17 12:59:55 -0700

    tableview and response for applegpt works

--- material-react-app/src/layouts/AppleMusicGPT/index.js
+++ material-react-app/src/layouts/AppleMusicGPT/index.js
@@ -15,7 +15,7 @@ Coded by www.creative-tim.com
 
 // @mui material components
 import Grid from "@mui/material/Grid";
-
+import React, { useState, useEffect } from 'react';
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 
@@ -28,31 +28,65 @@ import Footer from "examples/Footer";
 import MDInput from "components/MDInput";
 
 import MDButton from "components/MDButton";
-
+import GPTService from 'services/gpt-service';
+import Stack from '@mui/material/Stack';
+import { DataGrid } from '@mui/x-data-grid';
 const sendData = async () => {
 
-  const payload = "Make me a 5 song r and b playlist";
+  const payload = {
+    prompt: "Make me a 5 song r and b playlist"
+  };
 
   try {
-    const response = await fetch(`/generate-text`, {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ prompt: payload }) 
-  });
-    console.log(response); // Do something with the response
+    const response = await GPTService.generate(JSON.stringify(payload));
+    setResponseData(response); // Do something with the response
   } catch (error) {
     console.error(error);
   }
 };
 function AppleMusicGPT() {
+  const sendData = async () => {
 
+    const payload = {
+      prompt: "Make me a 5 song r and b playlist"
+    };
+  
+    try {
+      const response = await GPTService.generate(JSON.stringify(payload));
+      setResponseData(response); // Do something with the response
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const [isGenerated, setIsGenerated] = useState(false);
+  // State to hold response data
+  const [response, setResponseData] = useState(null);
+  useEffect(() => {
+    // If button has been clicked, call sendData
+    if(isGenerated) {
+      sendData(setResponseData);
+    }
+  }, [isGenerated]);
+  const columns = [
+    { field: 'song', headerName: 'Song Name', width: 200 },
+    { field: 'artist', headerName: 'Artist', width: 200 },
+  ];
 
+  // Ma to rows for DataGrid
+  const rows = response ? response.songs.map((song, index) => ({
+    id: index,
+    ...song
+  })) : [];
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox px={2} py={2}>
+      <Stack
+  direction="column"
+  justifyContent="center"
+  alignItems="center"
+  spacing={2}
+>
       <Grid container spacing={3}>
       <Grid item xs={7}>
       <MDInput label="Enter Your Prompt" multiline rows={5} fullWidth/>
@@ -64,18 +98,30 @@ function AppleMusicGPT() {
                   color="info"
                   fullWidth
                   type="button"
-                  onClick={sendData}
+                  onClick={() => setIsGenerated(true)} 
                 >
-                  Log Out
+                  Generate
                 </MDButton>
               </MDBox>
       </Grid>
     </Grid>
         
-        <MDBox mt={4.5}>
-          
+        <MDBox>
+        {response && ( // Only render DataGrid if response exists
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+              },
+            }}
+            pageSizeOptions={[5, 10]}
+            checkboxSelection
+          />
+        )}
         </MDBox>
-        
+        </Stack>
       </MDBox>
       <Footer />
     </DashboardLayout>
--- node-api/src/routes/gptapi/index.js
+++ node-api/src/routes/gptapi/index.js
@@ -17,8 +17,10 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 router.post('/generate-text', async (req, res) => {
     const sysprompt = "you are AI playlist generator";
-    const userprompt = req.query.prompt || '';
+
+    const userprompt = req.body.prompt || '';
     console.log(sysprompt)
+    console.log(userprompt)
     const completion = await openai.createChatCompletion({
         model: "gpt-4",
         messages: [{ "role": "system", "content": sysprompt }, { "role": "user", "content": `${userprompt} make the songs in this format 1. "song" by artist and add a playlist name at the end in the format PlaylistName: the name of the playlist`  }],
@@ -32,7 +34,55 @@ router.post('/generate-text', async (req, res) => {
     });
     console.log(completion.data.choices[0].message.content);
     const generatedText = completion.data.choices[0].message.content;
-    res.send(generatedText)
+
+    let pattern = /(\d+\.\s)(.*?)(\s-\s)(".*?")/;
+    let pattern2 = /(\d+\.\s)(".*?")(\sby\s)(.*)/;
+
+    let lines = generatedText.split('\n');
+
+    let regexedLines = [];
+
+    let playlist = {
+        playlistName: "",
+        songs: []
+    };
+    for(let line of lines){
+        let match = line.match(pattern);
+        let match2 = line.match(pattern2);
+       
+        const match3 = generatedText.match(/PlaylistName:\s*(.+)/);
+
+        if (match) {
+            let artist = match[2];
+            let song = match[4].replace(/"/g, '');
+           
+            regexedLines.push(`Song: ${song}, Artist: ${artist}`);
+            playlist.songs.push({
+                song: song,
+                artist: artist
+            });
+            if(match3){
+                playlist.playlistName = match3[1]
+                
+            }
+        }
+        else if (match2) {
+            let song = match2[2].replace(/"/g, '');
+            let artist = match2[4];
+            console.log(`Song: ${song}, Artist: ${artist}`);
+            playlist.songs.push({
+                song: song,
+                artist: artist
+            });
+          
+            if(match3){
+                playlist.playlistName = match3[1]
+
+            }
+        }
+    }
+console.log(playlist)
+    res.send(playlist);
 });
 
 
